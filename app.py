563f0f014a73a34be1245c7a4429564fd0d6cc95
Saloni Tanmor <138001726+Saloni3494@users.noreply.github.com>
2024-08-02 21:50:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, jsonify 
+import requests
+import pickle
+import numpy as np
+import sklearn
+from sklearn.preprocessing import StandardScaler
+import assist
+
+app = Flask(__name__)
+model = pickle.load(open('disease_model.pkl', 'rb'))
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        
+        symptoms = request.form.getlist('symptoms[]')
+        symptom_array = np.zeros(132) 
+        for i in range(0,132):
+            if assist.S_p[i] in symptoms:
+                symptom_array[i] = 1
+            else:
+                symptom_array[i] = 0
+                
+
+        # Make the prediction using the loaded model
+        input_data = symptom_array.reshape(1,-1)
+        prediction = model.predict(input_data)
+        disease = assist.Z_p[np.where(assist.Y_p==prediction[0])[0][0]]
+
+        
+
+        predicted_disease = f"Prediction: You may have {disease}."  
+
+        return render_template('index.html', prediction_text=predicted_disease)
+    else:
+        return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
